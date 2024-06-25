449cee3f9c7ea92da2ea9537f6a74b824bf0cded
Vinces <vince.egos@gmail.com>
2024-06-25 03:35:12 +0200

    feat: update interface

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { StrictMode } from "react";
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 
 const root = createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
+
+// 🥚
+console.info('%c🚨 ️Never Copy/Paste anything here', 'color:#8b0000;font-size:1rem;font-weight:bold')
+console.info('%c</💻> with ❤ by @vinces. Have Fun 🍻', 'color:#8fc471;font-size:1.1rem;font-weight:bold')
